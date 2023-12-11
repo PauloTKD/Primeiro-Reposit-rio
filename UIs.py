c366117cbddfd811eb737cb77fd40e96093c7ccd
PauloTKD <paulo-6447525@educar.rs.gov.br>
2023-12-11 13:59:36 -0300

    fort commit

--- UIs.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-window = tk.Tk()
-window.title('TKInter py app')
-window.geometry('300x300')
-newlabel = tk.Label(text = 'hello, TKInter!"')
-newlabel.grid(column = 1,row = 3)
-newlabel.pack(anchor='center')
-
-window.mainloop()
